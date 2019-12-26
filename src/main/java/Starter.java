9236c960c8b90bbe81e7916be9dd08273c55f71e
Andrey Ziabko <azhub@hotmail.com>
2019-12-26 12:53:22 +0300

    add set of threads in starter

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -3,9 +3,23 @@ public class Starter {
         Lot[] lots = {new Lot(0, 50),
                 new Lot(1, 100),
                 new Lot(2, 120)};
+        Thread buyer1 = new Thread(new Buyer(), "Buyer1");
+        Thread buyer2 = new Thread(new Buyer(), "Buyer2");
+        Thread buyer3 = new Thread(new Buyer(), "Buyer3");
 
-        new Thread(new Buyer()).start();
-        new Thread(new Buyer()).start();
-        new Thread(new Buyer()).start();
+        for (Lot lot : lots) {
+            buyer1.start();
+            buyer2.start();
+            buyer3.start();
+
+            try {
+                buyer1.join();
+                buyer2.join();
+                buyer3.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
