1248801af7f9bde48b9ad48394cef973907fd490
Andrey Ziabko <azhub@hotmail.com>
2020-01-07 15:41:06 +0300

    codewars task Maskify

--- src/test/java/codeWars/samples/MaskifyTest.java
+++ src/test/java/codeWars/samples/MaskifyTest.java
@@ -1,7 +1,15 @@
 package codeWars.samples;
 
 public class MaskifyTest {
+    public static void main(String[] args) {
+        System.out.println(maskify("sdfgh"));
+    }
     public static String maskify(String str) {
-        throw new UnsupportedOperationException("Unimplemented");
+       if(str.length() <= 4)
+           return str;
+       else {
+           String mask = str.replaceAll(".", "#").substring(0, str.length() - 4);
+           return mask + str.substring(str.length() - 4);
+       }
     }
 }
